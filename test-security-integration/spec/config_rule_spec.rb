93a7a649b8062d0554d4eb3a70d1767131310e35
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-09-20 17:09:07 -0400

    lint, conform to rubocop

--- test-security-integration/spec/config_rule_spec.rb
+++ test-security-integration/spec/config_rule_spec.rb
@@ -4,40 +4,43 @@ require 'json'
 describe('all_config_rules') do
   it 'have a status of COMPLIANT' do
     client = Aws::ConfigService::Client.new(region: 'us-east-1')
-    s3 = Aws::S3::Client.new(region: 'us-east-1')
+    # s3 = Aws::S3::Client.new(region: 'us-east-1')
 
-    #Get Config rules
-    rules = client.describe_config_rules({
+    # Get Config rules
+    rules = client.describe_config_rules(
       config_rule_names: [],
-      next_token: "",
-    })
+      next_token: ''
+    )
 
-
-    rule_stats = Array.new
+    rule_stats = []
     fail_count = 0
 
-    #Get compliance status for each rule
+    # Get compliance status for each rule
     rules.config_rules.each do |rule|
-      comp = client.describe_compliance_by_config_rule({
+      comp = client.describe_compliance_by_config_rule(
         config_rule_names: [rule.config_rule_name],
         compliance_types: [],
-        next_token: "",
-      })
-      comp_status = comp.compliance_by_config_rules[0].compliance.compliance_type
-      comp_result = comp_status == "COMPLIANT" ? "PASS" : "FAIL"
-      rule_stat = {"rule" => rule.config_rule_name, "status" => comp_status, "result" => comp_result}
+        next_token: ''
+      )
+      comp_status = comp
+                    .compliance_by_config_rules[0]
+                    .compliance.compliance_type
+      comp_result = comp_status == 'COMPLIANT' ? 'PASS' : 'FAIL'
+      rule_stat = { 'rule' => rule.config_rule_name,
+                    'status' => comp_status,
+                    'result' => comp_result }
       rule_stats.push(rule_stat)
-      if comp_status != "COMPLIANT"
-        fail_count = fail_count + 1
-      end
+      fail_count += 1 if comp_status != 'COMPLIANT'
     end
-    rule_stats.each {|rule| puts "#{rule["rule"]} is #{rule["status"]}:  #{rule["result"]}" }
-    status = fail_count == 0 ? "PASS" : "FAIL"
-    rule_stats_output = {"result" => status, "results" => rule_stats}
+    rule_stats.each do |rule|
+      puts "#{rule['rule']} is #{rule['status']}:  #{rule['result']}"
+    end
+    status = fail_count.zero? ? 'PASS' : 'FAIL'
+    rule_stats_output = { 'result' => status, 'results' => rule_stats }
     puts "cwd: #{Dir.pwd}"
-    File.open("sec_int_test_results.json","w") do |f|
+    File.open('sec_int_test_results.json', 'w') do |f|
       f.write(rule_stats_output.to_json)
     end
-    expect(status).to eq "PASS"
+    expect(status).to eq 'PASS'
   end
 end
